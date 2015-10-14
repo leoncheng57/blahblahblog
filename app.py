8ba7d8b747f05309b3855bb83d0c7d2a2a2c5e54
serenadipity <serenagchan@gmail.com>
2015-10-14 14:19:05 -0400

    Updated

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ def home():
 def login():
     return render_template("login.html")
 
+@app.route("/terms")
+def terms():
+    return render_template("terms.html")
+
 if __name__== "__main__":
     app.debug = True
     app.run(host='0.0.0.0',port=8000)
