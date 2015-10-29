54cbe6300c470fdb3f5fc2f52b72ae6ea003683c
backfrip <backfrip@users.noreply.github.com>
2015-10-29 23:38:00 +0000

    Just more refactoring, I suppose.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, session, redirect, url_for
-import Login, Register, Posts, Comments
+import Login, Register, Posts, Comments, utils
 import sqlite3, csv
 
 app = Flask(__name__)
@@ -18,41 +18,18 @@ def home():
         if len(Comments.retrieveComments(post[2])) != 0: 
             comments.append(Comments.retrieveComments(post[2]))
     if request.method == "POST":
-        #############################################
-        ### THIS TO BE MOVED TO SEPARATE FUNCTION ###
-        #############################################
-        # And maybe add an author search too...
-        # Actually, how is mongodb's builtin search capabilities?
+        # Maybe add an author search too...
+        # Actually, how are mongodb's builtin search capabilities?
         if 'search' in request.form: # This statement could be optimized...
-            posts_with_query = []
-            for post in Posts.retrievePost():
-                if request.form['query'] in post[0] or request.form['query'] in post[1]:
-                    # If the request form query is in post[0] or post[1]... (title and contents?)
-                    posts_with_query.append(post)
-            for comment_group in comments:
-                for comment in comment_group: # At the moment, I can only assume this is what is meant
-                    if request.form['query'] in comment[1]:
-                        is_unique_post = True
-                        #checks if the post the comment belongs to has already independently been added to posts.
-                        for post in posts_with_query:
-                            if post[2] == comment[0]:
-                                is_unique_post = False
-                        if(is_unique_post):
-                            for post in Posts.retrievePost():
-                                if post[2] == comment[0]:
-                                    posts_with_query.append(post)
             return render_template(
                 "searchresults.html",
-                LOGGEDIN = session['user'], # <-- Gotta fix this. Session parameter doesn't exist until log in!
-                POSTS = posts_with_query,
+                LOGGEDIN = session['user'],
+                POSTS = utils.search(request.form['query'], comments),
                 COMMENTS = comments,
                 QUERY = request.form['query'])
-        #############################################
-        ###               ENDBLOCK                ###
-        #############################################
         #all other POST requests require a login.
         if session['user'] == None or session["loggedin"] == False:
-            return redirect(url_for("home"))
+            return redirect('/')
         # Short-circuit evaluation for the win!
         if 'submitcomment' in request.form: # Really, the use of the 'in' keyword here bugs me...
             for post in Posts.retrievePost():
@@ -84,7 +61,6 @@ def home():
                 COMMENTS = comments) # Just noticed, this is not DRY in the *least*.
         title = request.form['title']
         cont = request.form['cont']
-        button = request.form['submitpost']
         Posts.makePost(title, cont, session['user'])
         return render_template(
             "home.html",
@@ -107,26 +83,26 @@ def home():
 
 @app.route("/login/", methods=['GET', 'POST'])
 def login():
-    if "loggedin" not in session:
-        session["loggedin"] = False
-    if request.method=="GET":
+    if 'user' not in session:
+        session['user'] = None
+    if 'loggedin' not in session:
+        session['loggedin'] = False
+    if request.method=='GET':
         return render_template("login.html")
-    else:
-        uname = request.form['user']
-        passw = request.form['pass']
-        button = request.form['button']
-        if Login.Login(uname, passw):
-            session["loggedin"] = True
-            session['user'] = uname
-            return redirect(url_for("home"))
-        else:
-            return render_template("login.html", NOTLOGGEDIN = "Error: Wrong username or password.")  
+    uname = request.form['user']
+    passw = request.form['pass']
+    button = request.form['button']
+    if Login.Login(uname, passw):
+        session["loggedin"] = True
+        session['user'] = uname
+        return redirect(url_for("home"))
+    return render_template("login.html", NOTLOGGEDIN = "Error: Wrong username or password.")  
 
 @app.route("/logout/")
 def logout():
-    session["loggedin"] = False
-    session["user"] = None
-    return redirect(url_for("home"))
+    session['loggedin'] = False
+    session['user'] = None
+    return redirect('/')
 			
 @app.route("/terms/")
 def terms():
@@ -136,36 +112,32 @@ def terms():
 def signup():
     if request.method=="GET":
         return render_template("signup.html")
-    else:
-        if request.form['pass'] != request.form['confirmpass']:
-            return render_template("signup.html", NOTLOGGEDIN = "Error: 'Password' and 'Confirm Password' do not match.")
-        elif len(request.form['user']) < 4 or len(request.form['pass']) < 8:
-            return render_template("signup.html", NOTLOGGEDIN = "Error: 'Username' must be at least 4 characters and 'Password' must be at least 8 characters.")
-        else: 
-            uname = request.form['user']
-            passw = request.form['pass']
-            button = request.form['button']			
-            if Register.Register(uname, passw):
-                return redirect(url_for("login"))
-            else:
-                return render_template("signup.html", NOTLOGGEDIN = "Error: Username already exists")
+    if request.form['pass'] != request.form['confirmpass']:
+        return render_template("signup.html", NOTLOGGEDIN = "Error: 'Password' and 'Confirm Password' do not match.")
+    if len(request.form['user']) < 4 or len(request.form['pass']) < 8:
+        return render_template("signup.html", NOTLOGGEDIN = "Error: 'Username' must be at least 4 characters and 'Password' must be at least 8 characters.")
+    username = request.form['user']
+    password = request.form['pass']
+    button = request.form['button']			
+    if Register.Register(username, password):
+        return redirect(url_for("login"))
+    return render_template("signup.html", NOTLOGGEDIN = "Error: Username already exists")
 
 @app.route("/myaccount/")
 def myaccount():
     if 'loggedin' in session and 'user' in session and session["loggedin"]:
-		return render_template("myaccount.html", LOGGEDIN = session['user'])
-    else:
-		return(render_template("myaccount.html"))
+        return render_template("myaccount.html", LOGGEDIN = session['user'])
+    return(render_template("myaccount.html"))
 		
 @app.route("/about/")
 def about():
-    if 'loggedin' in session and 'user' in session and session["loggedin"]:
-		return render_template("about.html", LOGGEDIN = session['user'])
-    else:
-		return(render_template("about.html"))
+    if 'loggedin' in session and 'user' in session and session['loggedin']:
+        # No, see, this check shouldn't ve necessary. It should be coded the same regardless.
+        return render_template("about.html", LOGGEDIN = session['user'])
+    return(render_template("about.html"))
 
 
 if __name__== "__main__":
     app.debug = True
     app.secret_key = "Password"
-    app.run(host='0.0.0.0',port=8000)
+    app.run(host='0.0.0.0', port=8000)
--- utils.py
+++ utils.py
@@ -1,30 +1,21 @@
-import csv
-import sqlite3
-import datetime
-import time
+import Posts
 
-def Register(username, password, ID):
-    conn = sqlite3.connect("backend")
-    c = conn.cursor()
-    uname = username
-    pword = password
-    blah = ID
-    params = (uname, pword, blah)
-    c.execute("INSERT INTO users VALUES(?,?,?)", params)
-    conn.commit()
-
-
-
-def add_post(title, body, id, points, user):
-    conn = sqlite3.connect("blog.db")
-    c = conn.cursor()
-    Title = title
-    Body = body
-    ID = id
-    p = points
-    d = str(datetime.date.today())
-    user = user
-    params = (Title, Body, ID, p, d, user)
-    q = ("INSERT INTO posts VALUES(?,?,?,?,?,?,?)", params)
-    c.execute(q)
-    conn.commit()
+def search(query, comments):
+    posts_with_query = []
+    for post in Posts.retrievePost():
+        if query in post[0] or query in post[1]:
+            # If the request form query is in post[0] or post[1]... (title and contents?)
+            posts_with_query.append(post)
+    for comment_group in comments:
+        for comment in comment_group: # At the moment, I can only assume this is what is meant
+            if query in comment[1]:
+                is_unique_post = True
+                #checks if the post the comment belongs to has already independently been added to posts.
+                for post in posts_with_query:
+                    if post[2] == comment[0]:
+                        is_unique_post = False
+                if(is_unique_post):
+                    for post in Posts.retrievePost():
+                        if post[2] == comment[0]:
+                            posts_with_query.append(post)
+    return posts_with_query
